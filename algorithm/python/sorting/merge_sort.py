ee4af1c7f8b16f8dc723295eb31cfd7fb192fcf1
Long9725 <develop9725@gmail.com>
2022-03-29 12:05:04 +0900

    Add: merge_sort.py

--- algorithm/python/sorting/merge_sort.py
+++ algorithm/python/sorting/merge_sort.py
@@ -10,4 +10,35 @@ def mergeSort(l):
     right = mergeSort(l[mid:])
     return merge(left, right)
 
-l = [38, 27, 43, 3, 9, 82, 10]
+def merge(left, right):
+    i = 0
+    j = 0
+    sorted_list = []
+
+    while(i < len(left)) & (j < len(right)):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+    
+    while (i < len(left)):
+        sorted_list.append(left[i])
+        i += 1
+
+    while (j < len(right)):
+        sorted_list.append(right[j])
+        j += 1
+    
+    return sorted_list
+# import 하려면 main 함수를 주석처리
+# import insertion_sort.py
+def main():
+    print("merge_sort")
+    l = [38, 27, 43, 3, 9, 82, 10]
+    l = mergeSort(l)
+    print(l)
+
+if __name__ == "__main__":
+    main()
